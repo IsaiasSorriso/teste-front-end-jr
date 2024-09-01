76d1c28219505602e8101f2ec33e82c38c6c6be4
Isaías Belarmina <149633447+IsaiasSorriso@users.noreply.github.com>
2024-09-01 17:44:24 +0000

    Update Scss Banner

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import '../styles/styles.scss'
+import '../styles/banner.scss'
 
 const Banner: React.FC = () => {
   return (
     <section className="banner">
-      <div className="container">
-        <div className="letras">
-          <h1 className="textPromo">Venha conhecer nossas promoções</h1>
-          <p className="textDesconto">50% Off nos produtos</p>
-          <a href="#" className="btn-promo">Ver produto</a>
-        </div>
+      <div className="banner__promotion">
+        <h1 className="textPromo">Venha conhecer nossas promoções</h1>
+        <p className="textDesconto">50% Off nos produtos</p>
+        <a href="#" className="btn-promo">Ver produto</a>
       </div>
     </section>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -41,7 +41,7 @@ const Header: React.FC = () => {
         <div className="header__logo">
           <img src={logo} alt="VTEX Logo" />
         </div>
-        <div className="search">
+        <div className="header__search-search">
           <input
             type="search"
             className="header__search-input"
@@ -68,8 +68,8 @@ const Header: React.FC = () => {
         </div>
       </div>
       <div className="header__bottom">
-        <nav className="header__menu">
-          <ul className="header__menu-list">
+        <nav className="header__bottom-menu">
+          <ul className="header__bottom-menu-list">
             <li>
               <a href="#" className="header__menu-item">
                 Todas Categorias
